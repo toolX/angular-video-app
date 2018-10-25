efdd2105aa456b27b2db6df3239384154abec18d
Philip Shamsiev <Philip_Shamsiev@epam.com>
2018-10-25 17:56:03 +0300

    Create video list data and bind it with the view

--- src/app/video-center/video-center.component.ts
+++ src/app/video-center/video-center.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Video } from './../video';
 
 @Component({
   selector: 'app-video-center',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./video-center.component.css']
 })
 export class VideoCenterComponent implements OnInit {
+
+  videos: Video[] = [
+    { "_id": "1", "title": "Title1", "url": "url1", "description": "Description1" },
+    { "_id": "2", "title": "Title2", "url": "url2", "description": "Description2" },
+    { "_id": "3", "title": "Title3", "url": "url3", "description": "Description3" }
+  ]
 
   constructor() { }
 
