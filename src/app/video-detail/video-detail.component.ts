886a4f00b496c3ecbef7136d06a65ddffe035946
Philip Shamsiev <Philip_Shamsiev@epam.com>
2018-10-26 10:42:04 +0300

    Add markup for video detail component, add show/hide title field functionality

--- src/app/video-detail/video-detail.component.ts
+++ src/app/video-detail/video-detail.component.ts
@@ -3,13 +3,24 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-video-detail',
   templateUrl: './video-detail.component.html',
-  styleUrls: ['./video-detail.component.css']
+  styleUrls: ['./video-detail.component.css'],
+  inputs: ["video"]
 })
 export class VideoDetailComponent implements OnInit {
+
+  private editTitle: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngOnChanges() {
+    this.editTitle = false;
+  }
+
+  onTitleClick() {
+    this.editTitle = true;
   }
 
 }
